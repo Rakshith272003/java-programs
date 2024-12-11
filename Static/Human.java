af980ca317e5f71b20426df0377981cf0c624282
Rakshith272003 <rakshithmdpura@gmail.com>
2024-12-11 09:47:15 +0530

    learnings

--- Static/Human.java
+++ Static/Human.java
@@ -5,12 +5,14 @@ public class Human {
     String name;
     char gender;
     boolean married;
+    static long population;
 
     public Human(int age, char gender, boolean married, String name) {
         this.age = age;
         this.gender = gender;
         this.married = married;
         this.name = name;
+        Human.population+=1;
     }
 }
 
--- Static/Main.java
+++ Static/Main.java
@@ -2,8 +2,11 @@ package Static;
 
 public class Main {
     public static void main(String[] args) {
-        Human Rakshith = new Human(22,'M',false,"Rakshith");
-        System.out.println(Rakshith.name);
+        //Human Rakshith = new Human(22,'M',false,"Rakshith");
+        //System.out.println(Rakshith.name);
+
+        //we can access static member without creating any object
+        System.out.println(Human.population);
 
 
     }
