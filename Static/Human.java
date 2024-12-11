efffa677b9028ea252b5d328bbdcdae37dfc0998
Rakshith272003 <rakshithmdpura@gmail.com>
2024-12-11 09:12:38 +0530

    learnings

--- Static/Human.java
+++ Static/Human.java
@@ -1,4 +1,16 @@
 package Static;
 
 public class Human {
+    int age;
+    String name;
+    char gender;
+    boolean married;
+
+    public Human(int age, char gender, boolean married, String name) {
+        this.age = age;
+        this.gender = gender;
+        this.married = married;
+        this.name = name;
+    }
 }
+
---
+++ Static/Main.java
@@ -0,0 +1,11 @@
+package Static;
+
+public class Main {
+    public static void main(String[] args) {
+        Human Rakshith = new Human(22,'M',false,"Rakshith");
+        System.out.println(Rakshith.name);
+
+
+    }
+
+}
