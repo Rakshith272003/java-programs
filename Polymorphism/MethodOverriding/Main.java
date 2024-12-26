bd173a91dcd1c433e9fa7d19cb11d8b99be09b67
Rakshith272003 <rakshithmdpura@gmail.com>
2024-12-26 12:31:39 +0530

    learnings

--- Polymorphism/MethodOverriding/Main.java
+++ Polymorphism/MethodOverriding/Main.java
@@ -3,6 +3,7 @@ package Polymorphism.MethodOverriding;
 public class Main {
     public static void main(String[] args) {
         Animal animal;
-        animal =
+        animal = new Dog();
+        animal.sound();
     }
 }
